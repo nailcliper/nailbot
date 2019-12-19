c82a6ab8f08cebffe46f025d33b3f277ca209562
nailcliper <ltayanami@hotmail.com>
2019-12-18 22:03:10 -0800

    Move socket send/receive to threads

--- app.py
+++ app.py
@@ -1,5 +1,95 @@
-from twitch_socket import s, parseData, processData
-from globals import HOST, PORT, NICK, PASS, CHAN
+from twitch_socket import s
+from globals import HOST, PORT, NICK, PASS, CHAN, RECV_QUEUE
+from notice import handle_NOTICE
+from privmsg import handle_PRIVMSG
+from usernotice import handle_USERNOTICE
+
+def parseData(line):
+    data = {}
+    lines = line.split(' ')
+    #Parse Tags
+    tags = {}
+    if lines[0][0] == '@':
+        temp = lines.pop(0)
+        temp = temp[1:].split(";")
+        for tag in temp:
+            kv = tag.split('=')
+            tags[kv[0]] = kv[1]
+    #end if
+    data['tags'] = tags
+    
+    #Parse prefix
+    data['prefix'] = ''
+    if  lines[0][0] == ':':
+        data['prefix'] = lines.pop(0)[1:]
+    #end if
+    
+    #Parse Command and Args
+    data['command'] = lines.pop(0)
+    data['args'] = []
+    while lines:
+        if lines[0][0] != ':':
+            data['args'].append(lines.pop(0))
+        else:
+            break
+    
+    #Parse Message
+    message = ''
+    while lines:
+        message += ' ' + lines.pop(0)
+    data['message'] = message[2:]
+    
+    return data
+#end def
+
+def processData(data):
+    command = data['command']
+    if not command:
+        pass
+    
+    #Twitch IRC: Membership
+    #JOIN: Join a specified chat room in a channel
+    
+    #MODE: Gain/lose moderator status in a channel
+    
+    #NAMES: List current chatters in a channel
+    
+    #PART: Depart from a channel
+    
+    #Twitch IRC: Commands:
+    #CLEARCHAT: Purgeg a user's message(s), typically after a user is banned from chat or timed out
+    
+    #CLEARMSG: Single message removal on a channel. This is triggered via /delete <target-msg-ig> on IRC
+    
+    #HOSTTARGET: Channel starts or stops host mode
+   
+    #NOTICE: General notices from the server
+    elif command == "NOTICE":
+        handle_NOTICE(data)
+    
+    #RECONNECT: Rejoin channels after a restart
+    
+    #ROOMSTATE: Identifies the channel's chat settings (e.g. slow mode duration)
+    
+    #USERNOTICE: Announces Twitch-specific events to the channel (e.g. subscription notification)
+    elif command == "USERNOTICE":
+        handle_USERNOTICE(data)
+    
+    #USERSTATE: Identifies a user's chat settings or properties (e.g. chat color)
+    
+    #Twitch IRC: Tags
+    #GLOBALUSERSTATE Provides data about the current logged-in user
+    
+    #PRIVMSG: Send a message to a channel
+    elif command == "PRIVMSG":
+        handle_PRIVMSG(data)
+    
+    
+    #PING: The server will send you a PING :tmi.twitch.tv. 
+    #      To ensure that your connection to the server is not prematurely terminated, reply with PONG :tmi.twitch.tv.
+    elif command == "PING":
+        s.send("PONG :tmi.twitch.tv")
+#end def
 
 s.connect(HOST,PORT)
 s.send("PASS " + PASS)
@@ -8,13 +98,14 @@ s.send("CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands")
 s.join_channel(CHAN)
 
 while True:
-    lines = s.recv_all().split('\r\n')
-    for line in lines:
-        if len(line) > 0:
-            data = {}
-            print("<- " + line)
-            
-            data = parseData(line)
-            print("Data: \n",data,'\n')
-            
-            processData(data)
+    while(len(RECV_QUEUE) > 0):
+        lines = RECV_QUEUE.pop().split('\r\n')
+        for line in lines:
+            if len(line) > 0:
+                print("<- " + line)
+                data = {}
+                data = parseData(line)
+                print("Data: \n",data,'\n')
+                
+                processData(data)
+#end while
--- twitch_socket.py
+++ twitch_socket.py
@@ -1,98 +1,47 @@
 import socket
-from globals import BUF_SIZE
+import threading
+from globals import BUF_SIZE, SEND_QUEUE, RECV_QUEUE
 
 class Socket:
     def __init__(self):
         self.s = socket.socket()
+        self.connected = False
+        self.receiver = threading.Thread(target=self.recv_all)
+        self.receiver.setDaemon(True)
+        self.sender = threading.Thread(target=self.send_all)
+        self.sender.setDaemon(True)
 
     def connect(self, host, port):
         self.s.connect((host,port))
+        self.receiver.start()
+        self.sender.start()
 
     def recv_all(self):
-        data = ''
         while True:
-            data += self.s.recv(BUF_SIZE).decode('utf-8')
-            if data[-2:] == '\r\n':
-                return data
+            data = ''
+            while data[-2:] != '\r\n':
+                data += self.s.recv(BUF_SIZE).decode('utf-8')
+            RECV_QUEUE.append(data)
     #end def
     
-    def send(self, message):
-        self.s.send(bytes(message + '\r\n',"UTF-8"))
-        print("-> " + message + '\n')
-
+    def send_all(self):
+        while True:
+            while len(SEND_QUEUE) > 0:
+                message = SEND_QUEUE.pop(0)
+                self.s.send(bytes(message + '\r\n',"UTF-8"))
+                print("-> " + message + '\n')
+    #end def
+    
+    def send(self, m):
+        SEND_QUEUE.append(m)
+    
     def send_msg(self, message, channel):
-        self.send("PRIVMSG #" + channel +  ":" + message)
+        m = "PRIVMSG #" + channel + " :" + message
+        SEND_QUEUE.append(m)
 
     def join_channel(self,  channel):
-        self.send("JOIN #" + channel)
+        m = "JOIN #" + channel
+        SEND_QUEUE.append(m)
 #end class
 
 s = Socket()
-
-def parseData(line):
-    data = {}
-    lines = line.split(' ')
-    #Parse Tags
-    tags = {}
-    if lines[0][0] == '@':
-        temp = lines.pop(0)
-        temp = temp[1:].split(";")
-        for tag in temp:
-            kv = tag.split('=')
-            tags[kv[0]] = kv[1]
-    #end if
-    data['tags'] = tags
-    
-    #Parse prefix
-    data['prefix'] = ''
-    if  lines[0][0] == ':':
-        data['prefix'] = lines.pop(0)[1:]
-    #end if
-    
-    #Parse Command and Args
-    data['command'] = lines.pop(0)
-    data['args'] = []
-    while lines:
-        if lines[0][0] != ':':
-            data['args'].append(lines.pop(0))
-        else:
-            break
-    
-    #Parse Message
-    message = ''
-    while lines:
-        message += ' ' + lines.pop(0)
-    data['message'] = message[2:]
-    
-    return data
-#end def
-
-def processData(input):
-    command = input['command']
-    if not command:
-        pass
-    
-    #GLOBALUSERSTATE Provides data about the current logged-in user
-    #PRIVMSG: Send a message to a channel
-    
-    #JOIN: Join a specified chat room in a channel
-    #MODE: Gain/lose moderator status in a channel
-    #NAMES: List current chatters in a channel
-    #PART: Depart from a channel
-    
-    #CLEARCHAT: Purgeg a user's message(s), typically after a user is banned from chat or timed out
-    #CLEARMSG: Single message removal on a channel. This is triggered via /delete <target-msg-ig> on IRC
-    #HOSTTARGET: Channel starts or stops host mode
-    #NOTICE: General notices from the server
-    #RECONNECT: Rejoin channels after a restart
-    #ROOMSTATE: Identifies the channel's chat settings (e.g. slow mode duration)
-    #USERNOTICE: Announces Twitch-specific events to the channel (e.g. subscription notification)
-    #USERSTATE: Identifies a user's chat settings or properties (e.g. chat color)
-    
-    #PING: The server will send you a PING :tmi.twitch.tv. 
-    #      To ensure that your connection to the server is not prematurely terminated, reply with PONG :tmi.twitch.tv.
-    elif command == "PING":
-        s.send("PONG :tmi.twitch.tv")
-    
-    pass
-#end def
